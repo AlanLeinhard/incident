e14eb3f09edae8da14dc67dc54373458f86dbb2f
Alan Leinhard <61091228+AlanLeinhard@users.noreply.github.com>
2022-04-01 12:12:30 +0300

    step

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from ast import For
+import cmd
 import imp
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication
-import subprocess
+from subprocess import Popen, PIPE
+# from subprocess import PIPE
 
 Form, Window = uic.loadUiType("script.ui")
 
@@ -12,20 +14,47 @@ form = Form()
 form.setupUi(window)
 window.show()
 
-def onclick():
-    # print(form.plainTextEdit.toPlainText())
-    result = subprocess.run('ls', capture_output=True)
+def call_cmd(cmd):
+    proc = Popen(cmd, stdout=PIPE, stderr=PIPE, shell=True, encoding='cp866')
+    stdout, stderr = proc.communicate()
+    output = stdout + stderr
+    return output
 
-    form.plainTextEdit.insertPlainText(str(result) + "\n")
-    form.listWidget.addItem("jhgjfhdghj")
+
+def onclick():
+    cmd = ['wmic', 'logicaldisk', 'get']
+    # cmd = ['diskpart', '<', 'diskpart.txt']
+    output = call_cmd(cmd)
+    form.listWidget.clear()
+    form.plainTextEdit.setPlainText(str(output) + "\n")
+
+    i = 0
+    j = 0
+    for line in output.splitlines():
+        # if line.startswith('  Диск ') == True and  line.startswith('  Диск #') == False:
+        form.listWidget.addItem(line)
+        form.tableWidget.setRowCount(1)
+        j = 0
+        for word in line.split(' '):
+            # print(word)
+            item = form.tableWidget.item(i, j)
+            item.setText(word)
+            form.tableWidget.insertItem(i, j, item)
+            j = j + 1
+        i = i + 1
+
+def cmd_click():
+    cmmd = form.plainTextEdit_2.toPlainText()
+    form.plainTextEdit.setPlainText(str(call_cmd(cmmd)) + "\n")
+    
 
 def item_click():
     form.plainTextEdit.insertPlainText("click item:" + str(form.listWidget.currentItem().text()) + "\n")
     form.listWidget.currentItem().setText("qwertyuiop")
-    # print(form.listWidget.selectedItems().mimeData())
 
 
 form.pushButton.clicked.connect(onclick)
+form.pushButton_2.clicked.connect(cmd_click)
 form.listWidget.clicked.connect(item_click)
 
 app.exec_()
